5ee07a7aaf8ea1aca53862e3104ce01ff000748d
zhaojian@clog.com <250529492@qq.com>
2023-02-07 00:48:05 +0800

    feat: redux简单版

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Count from './components/Count'
+
+export default class app extends Component {
+  render() {
+    return (
+      <div>
+        <Count />
+      </div>
+    )
+  }
+}
